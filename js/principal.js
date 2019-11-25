51f2925d4e21e473a0dfa55fa5737a520f54da4c
Valmyr Tavares <valmyr.lima@millennium.com.br>
2019-11-25 15:01:50 -0300

    Aqui pegamos todos os valores nome peso gordura IMC altura. Tudo coletado e devolvido sendo que o IMC foi calculado a partir do peso e altura. Simples de bater o olho e entender

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,19 @@
 var paciente = document.querySelector("#primeiro-paciente");
 console.log(paciente)
-var peso = paciente.querySelector(".info-peso");
-console.log(peso)
+var tdNome = paciente.querySelector(".info-nome");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdGordura = paciente.querySelector(".info-gordura");
+var tdImc = paciente.querySelector(".info-imc");
+
+var nome = tdNome.textContent;
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+var gordura = tdGordura.textContent;
+var IMC = peso / (altura * altura);
+
+console.log(nome);
+console.log(peso);
+console.log(altura);
+console.log(gordura);
+console.log(IMC);
