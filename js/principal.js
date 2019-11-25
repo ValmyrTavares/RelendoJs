f688641562c8c81288e723524e13d7eab1f343cd
Valmyr Tavares <valmyr.lima@millennium.com.br>
2019-11-25 14:39:36 -0300

    Usando id para ser mais específico em que deve ser selecionado. Primeiro foi feito um id selecionado sobre a variável paciente e depois o peso é selecionado a partir do paciente atravez de uma classe .info-peso

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,4 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Mudou e mudou de novor"
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente)
+var peso = paciente.querySelector(".info-peso");
+console.log(peso)
